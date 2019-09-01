ee24415643acf3e8626860cf9aaf33d764ba1cfd
Paweł Gesek <pgesek@gmail.com>
2019-09-01 04:09:13 +0200

    Map generation for the second analysis

--- analysis/maps/delta_map.py
+++ analysis/maps/delta_map.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import geoplot
 import matplotlib.pyplot as plt
+import os
 
 
 FIELDS = ['t2mean2m_delta', 'tmax2m_delta', 'tmin2m_delta']
@@ -9,24 +10,31 @@ SUFFIXES = ['_avg', '_abs_avg', '_std_dev']
 TITLES = {'_avg':  'Średnia delta', '_abs_avg': 'Średnie delta wartości bezwzględnej',
           '_std_dev': 'Odchylenie standardowe'}
 
-FILES = ['all_data', '2018-10-27_to_2018-12-31',
-         '2019-01-01_to_2019-02-28', '2019-03-01_to_2019-04-01']
 
-
-def run():
+def run(map_dir_name, files):
 
     # europe = gpd.read_file('https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson')
     poland = gpd.read_file('https://gist.githubusercontent.com/filipstachura/391ecb779d56483c070616a4d9239cc7/raw'
                            '/b0793391ab0478e0d92052d204e7af493a7ecc92/poland_woj.json')
 
-    for file in FILES:
-        geo_file = 'geo/data/first_analysis/{file}.geojson.g'.format(file=file)
+    for file in files:
+        print('Generating maps for: ' + file)
+
+        geo_file = 'maps/geo/data/{dir_name}/{file}.geojson'\
+            .format(
+                dir_name=map_dir_name,
+                file=file
+            )
         data = gpd.read_file(geo_file)
+
+        print('Geo file loaded')
 
         for field in FIELDS:
             for suffix in SUFFIXES:
 
                 prop = field + suffix
+
+                print('Generating map for: ' + prop)
 
                 ax = geoplot.choropleth(data,
                                         hue=prop,
@@ -44,8 +52,13 @@ def run():
                 plt.title('Pole: {field}, {title}'.format(field=field,
                                                           title=TITLES[suffix]))
 
+                path = 'D:\\workspace\\MGR\\maps\\{dir_name}\\{file}'\
+                    .format(dir_name=map_dir_name, file=file)
+                os.makedirs(name=path, exist_ok=True)
+
                 fig = ax.get_figure()
-                fig.savefig('D:\\workspace\\MGR\\maps\\first_analysis\\{file}\\{prop}_ad.png'
-                            .format(file=file, prop=prop))
+                fig.savefig(path + '\\{prop}_ad.png'.format(prop=prop))
 
                 plt.close(fig)
+
+        print('Map generated')
--- analysis/run.py
+++ analysis/run.py
@@ -11,11 +11,21 @@ from maps.geo.generate_rectangles import run as generate_rectangles
 
 load_dotenv()
 
+FIRST_ANALYSIS_FILES = [
+    'all_data',
+    '2018-10-27_to_2018-12-31',
+    '2019-01-01_to_2019-02-28',
+    '2019-03-01_to_2019-04-01'
+]
+
 #run_first_query_set()
 #run_second_query_set('ksgmet-test', 'test')
-#generate_delta_maps()
+generate_delta_maps(
+    map_dir_name='second_analysis',
+    files=['ksgmet-test', 'ksgmet-may', 'ksgmet-november']
+)
 #generate_rectangles_map()
 #generate_region_map()
 #generate_geojson_first_analysis()
-generate_geojson_second_analysis('ksgmet-november', 'november')
+#generate_geojson_second_analysis('ksgmet-november', 'november')
 #generate_rectangles()
